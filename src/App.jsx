31e63ae0151fc50caec4b7661f76630aa17e97d3
Ghislano26 <ghislainatebachouacha@gmail.com>
2026-06-23 20:16:21 +0100

    form validation and navigation (react-router-dom)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from './components/Card'
+import { Link } from 'react-router-dom'
+import {z} from 'zod'
 // import avatar11 from './assets/avatar11.png'
 // import avatar12 from './assets/avatar12.png'
 // import avatar13 from './assets/avatar13.png'
+
+const joueurSchema = z
+.string()
+.min(3, {message: 'Minimum 3 caracteres'})
+.max(15, {message:'maximum 12 caracteres'})
 
 function App() {
   const [state, functionState] = useState([
@@ -12,6 +19,8 @@ function App() {
   ])
 
   const [name, setName] = useState('')
+  const [error, setError] = useState('')
+
 
   const handleChange = (e)=>{
     console.log(e.target.value);
@@ -20,6 +29,28 @@ function App() {
 
   const handleSubmit = (e)=>{
     e.preventDefault()
+    setError('')
+    const nameNettoye = name.trim()
+    const nameValid = joueurSchema.safeParse(nameNettoye)
+    if (!nameValid.success) {
+      setError(nameValid.error.format()._errors[0])
+      return
+    }
+
+    const copySate = [...state]
+    const id = new Date().getTime()
+    const nom = nameNettoye
+    copySate.push({id:id, name:nom})
+    functionState(copySate)
+
+    setName('')
+    
+  }
+
+  const handleDelete = (id)=>{
+    const state1= [...state]
+    const newState = state1.filter(e=>e.id !== id)
+    functionState(newState)
   }
 
   return (
@@ -34,19 +65,22 @@ function App() {
          />
         <button
           className='px-4 py-2 bg-blue-500 text-white ml-5 rounded transition-all cursor-pointer hover:scale-95'>Enregistrer</button>
+        
       </form>
+      {error && <span className='text-red-500'>{error}</span> }
 
-      <div className='w-200 flex flex-col justify-center items-center p-8  bg-green-200'>
-        <h1 className='font-bold text-2xl'>Vos grands joueurs</h1>
+      <div className='w-200 flex flex-col justify-center items-center p-8  gap-8 bg-green-200'>
+        <h1 className='font-bold text-2xl'>Vos grands joueurs de foot</h1>
       <ul>{state.map( (e)=>(
-        <li className='mt-5' key={e.id}>{e.name}</li>
+        <div key={e.id} className='mt-5 flex justify-center items-center gap-8'>
+          <li className=''>{e.name}</li>
+          <button className='px-4 py-2 p-2 bg-green-600 rounded text-white transition-all cursor-pointer hover:scale-95' onClick={()=>handleDelete(e.id)}>Delete</button>
+        </div>
+          
+        
       ))}</ul>
-
-
-      {/* <Card name={'Bertrand'} metier={'Pousseur'} experience={4} logo={avatar11}/>
-      <Card name={'Bertrand'} metier={'Pousseur'} experience={4} logo={avatar12}/>
-      <Card name={'Bertrand'} metier={'Pousseur'} experience={4} logo={avatar13}/> */}
     </div>
+    <Link to={'/'}>Accueil</Link>
     </main>
     
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import Home from './Home.jsx'
+
+
+ 
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: '/joueurs',
+    element: <App/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}></RouterProvider>
   </StrictMode>,
 )
